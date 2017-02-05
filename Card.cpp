3071ae9569c3496af7fffa7d6e5a597255960615
adambechtold <adamcbechtold@gmail.com>
2017-02-05 11:48:28 -0500

    Initial commit

---
+++ Card.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Adam Bechtold on 2/5/17.
+//
+
+#include "Card.h"
+
+Card::Card(int value, string suite)
+// constructor for a card
+{
+    this->value = value;
+    this->suite = suite;
+}
+
+void Card::setValue(int value)
+// set the value of a playing card
+{
+    this->value = value;
+}
+
+void Card::setSuite(string suite)
+// set the suite of the playing card
+{
+    this->suite = suite;
+}
+
+int Card::getValue() const
+// get the value of this playing card
+{
+    return this->value;
+}
+
+string Card::getSuite() const
+// return the suite of this playing card
+{
+    return this->suite;
+}
+
+ostream &operator<<(ostream& ostr, const Card &c)
+// overload << operator to print fields of the card
+{
+    ostr << " " << c.getValue() << " of " << c.getSuite();
+    return ostr;
+}
---
+++ Card.h
@@ -0,0 +1,33 @@
+//
+// Created by Adam Bechtold on 2/5/17.
+//
+
+#include <string>
+
+using namespace std;
+
+#ifndef PROJECT2_CARD_H
+#define PROJECT2_CARD_H
+
+// to represent playing cards
+class Card {
+private:
+    int value;
+    string suite;
+
+public:
+    Card(int value, string suite);
+
+    void setValue(int value);
+
+    void setSuite(string suite);
+
+    int getValue() const;
+
+    string getSuite()const;
+
+    friend ostream& operator<< (ostream& ostr, const Card& c);
+};
+
+
+#endif //PROJECT2_CARD_H
