d759ca2287c0fd86b6a459523a1aa608f2febfe1
adambechtold <adamcbechtold@gmail.com>
2017-02-06 14:59:29 -0500

    test in Card.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+#include "Card.h"
+
+using namespace std;
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    Card c = Card(3, "Hearts");
+
+    cout << c;
+
     return 0;
 }
