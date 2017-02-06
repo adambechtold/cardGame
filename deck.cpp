87953faebe267cc6b6db7636d21137ea0b1333cf
adambechtold <adamcbechtold@gmail.com>
2017-02-06 15:31:37 -0500

    test in Card.cpp

--- deck.cpp
+++ deck.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "deck.h"
+#include "Card.h"
+
+class Deck {
+
+
+    Deck::Deck() {
+        this->suits = ("Club", "Diamonds", "Hearts", "Spades");
+        // loop through an assign values
+    }
+
+    void Deck::shuffle() {
+        //shuffle some shit
+    }
+};
--- deck.h
+++ deck.h
@@ -2,10 +2,22 @@
 // Created by Patrick on 2/6/17.
 //
 
+#include <iostream>
+
 #ifndef DECK_H
 #define DECK_H
-class deck
-{
+
+class Deck {
+private:
+    node<Card> *front;
+
+    string suits[4];
+
+public:
+
+    Deck();
+
+    void shuffle();
 };
 
 #endif //PROJECT2_DECK_H
