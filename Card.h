9cfd9ffb537fa70605fc57ad19f13eb6a188adf3
adambechtold <adamcbechtold@gmail.com>
2017-02-11 21:59:31 -0500

    this thing is hotshit. Like literally the bee's knees. Los rodillos del abejorro.

--- Card.h
+++ Card.h
@@ -1,6 +1,16 @@
+// Project 2a
+// Adam Bechtold & Patrick Buzza
 //
-// Created by Adam Bechtold and Patrick Buzza on 2/5/17.
-//
+// Header file for the Card class
+// Contains the private fields that hold the playing card's value and suit.
+// Also includes declarations of the methods necessary to use the Cards in a
+// card game including:
+//   - a Constructor
+//   - setValue
+//   - setSuit
+//   - getValue
+//   - getSuit
+
 
 #include <iostream>
 #include <string>
